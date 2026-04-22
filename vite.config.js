c36534194b63395f3d261e8d5d5bff4999b8555c
이은서 <hg231049@gamil.com>
2026-04-23 08:51:42 +0900

    les commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 깃허브 레포지토리 이름을 여기에 적어주세요. 
+  // 앞뒤로 슬래시(/)를 꼭 붙여야 합니다!
+  base: '/weatherapp/', 
 })
